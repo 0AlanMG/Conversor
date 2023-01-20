3c82660ebf52ec54d1c83707a2a71fdefcf093b6
0AlanMG <esc.amdog5@gmail.com>
2023-01-20 17:14:08 -0600

    Se agrega Menu de Monedas y se valida el primer caso con switch

--- src/mx/conversor/main/Main.java
+++ src/mx/conversor/main/Main.java
@@ -20,7 +20,35 @@ public class Main {
 			
 			if(converter.toString() == "Conversor de Moneda") {
 				
-				System.out.println("A");
+				/*Menu de Monedas*/
+				Object[] currencyConversionOptions = { 
+						"De MXN (Peso) a USD (Dolar)", 
+						"De MXN (Peso) a EUR (Euro)", 
+						"De MXN (Peso) a GBP (Libra Esterlina)", 
+						"De MXN (Peso) a JPY (Yen Japonés)", 
+						"De MXN (Peso) a KRW (Won Surcoreano)", 
+						"De USD (Dolar) a MXN (Peso)", 
+						"De EUR (Euro) a MXN (Peso)", 
+						"De GBP (Libra Esterlina) a MXN (Peso)", 
+						"De JPY (Yen Japonés) a MXN (Peso)", 
+						"De KRW (Won Surcoreano) a MXN (Peso)",
+						};
+				Object currencyConversion = JOptionPane.showInputDialog(null, 
+						"Elije la moneda a la que deseas convertir tu dinero", 
+						"Monedas", 
+						JOptionPane.QUESTION_MESSAGE, 
+						null, 
+						currencyConversionOptions, 
+						currencyConversionOptions[0]
+						);
+				
+				switch (currencyConversion.toString()) {
+				case "De MXN (Peso) a USD (Dolar)": {
+					System.out.println("De MXN A USD");
+				}
+				default:
+					throw new IllegalArgumentException();
+				}
 				
 			}else if(converter.toString() == "Conversor de Temperatura") {
 				System.out.println("B");
